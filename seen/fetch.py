b94ef0ca8dbf5d32d3f15de99bd36d5a7c76594b
weiy <b754048538@163.com>
2018-03-10 17:40:55 +0800

    support GET and POST

--- seen/fetch.py
+++ seen/fetch.py
@@ -5,10 +5,13 @@ from .logger import logger
 
 async def fetch_content(url, session, **kwargs):
     method = kwargs.get('method') or 'GET'
-    response = await session.get(url, **kwargs)
-
+    if method == 'GET':
+        response = await session.get(url, **kwargs)
+    else:
+        response = await session.post(url, **kwargs)
+        
     if isinstance(response, ErrorRequest):
-        logger.error("url {} error, error information: {}".format(url, response.error_info))
+        logger.error("url {} is an error url, error information: {}".format(url, response.error_info))
         return None
         
     return response
