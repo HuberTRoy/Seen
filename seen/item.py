e7ad78f8cb1f5deda7708fb55a7bef7a69cced1e
weiy <b754048538@163.com>
2018-07-16 14:55:53 +0800

    translate partial URL to full URL

--- seen/item.py
+++ seen/item.py
@@ -3,42 +3,58 @@ import json
 from html import unescape
 
 from .logger import logger
-from .selector import Selector 
+from .selector import Selector
 
 
 class BaseItem(type):
-
+    """
+        The meta class for item.
+    """
     def __new__(cls, name, bases, namespace):
         selector = {}
         for name, value in namespace.items():
             if isinstance(value, Selector):
                 selector[name] = value
-        
+
         namespace['selector'] = selector
 
         return type.__new__(cls, name, bases, namespace)
 
 
-# get help of metaclass:
+# Get help of metaclass:
 # https://stackoverflow.com/questions/100003/what-is-a-metaclass-in-python
 class Item(metaclass=BaseItem):
+    """
+      class MyItem(Item):
+        # css rule
+        title = Css('title')
+
+        # asynchronous or synchronize
+        def save(self):
+            print(self.title)
+    """
 
     def __init__(self, spider, response, isJson=False):
-        html = response.text
+        self.html = response.text
         self.result = {}
         self.spider = spider
         self.response = response
         if isJson:
-            self.result['json'] = json.loads(html)
+            try:
+                self.result['json'] = json.loads(self.html)
+            except:
+                logger.error("The JSON contents returned by URL ({}) loaded failure, please check again.".format(
+                    response.url), exc_info=True)
         else:
-            html = unescape(html)
+            self.html = unescape(self.html)
 
             for name, selector in self.selector.items():
-                contents = selector.get_select(html)
+                contents = selector.get_select(self.html)
                 if contents is None:
-                    logger.error('selector "{}:{}" was error, please check again.'.format(name, selector))
+                    logger.error('selector "{}:{}" was error, please check again.'.format(
+                        name, selector), exc_info=True)
                     continue
-                
+
                 self.result[name] = contents
 
     def save(self):
@@ -49,7 +65,7 @@ class Item(metaclass=BaseItem):
 # save binary data.
 class BinItem(object):
 
-    def __init__(self, spider,  response):
+    def __init__(self, spider, response):
         self.response = response
         self.spider = spider
         self.content = response.content
@@ -57,5 +73,3 @@ class BinItem(object):
     def save(self):
 
         raise(TypeError('No save operation.'))
-
-
--- seen/parser.py
+++ seen/parser.py
@@ -1,7 +1,7 @@
 import re
 import asyncio
 from html import unescape
-
+from urllib.parse import urljoin, urlparse
 
 from .logger import logger
 from .item import BinItem
@@ -9,14 +9,16 @@ from .item import BinItem
 
 class BaseParser(object):
     """
-    :param rules: list[function(response)]/tuple(function(response)).
+    :param rules: list[function(response)]/tuple(function(response)). 
+                  To judge the Url is wanted to be or None to parse all Urls. None by default.
     :param item: Item. 
-    :param url_rule: str or function.
-                    if it is a str will use re.findall(url_rule, response.text)
-                    if it is a function will use url_rule(response.text), the function expects returning an iterable object that contains URL.
+    :param urlRule: str or function.
+                    if it is a str will use re.findall(urlRule, response.text)
+                    if it is a function will use urlRule(response.text), the function expects returning an iterable object that contains URL.
                     By default it is r'''(?i)href=["']([^\s"'<>]+)'''.
     """
-    def __init__(self, url_rule=None, item=None, rules=None, isJson=False):
+
+    def __init__(self, urlRule=None, item=None, rules=None, isJson=False):
         # which spider own it.
         self.spider = None
 
@@ -24,10 +26,10 @@ class BaseParser(object):
         self.item = item
         self.isJson = isJson
 
-        if url_rule is None:
-            url_rule = r'''(?i)href=["']([^\s"'<>]+)'''
+        if urlRule is None:
+            urlRule = r'''(?i)href=["']([^\s"'<>]+)'''
 
-        self.url_rule = url_rule
+        self.urlRule = urlRule
 
     def __call__(self, response):
         return self.analyze_response(response)
@@ -36,19 +38,47 @@ class BaseParser(object):
         item = self.item(self.spider, response, self.isJson)
         return item
 
-    def get_urls(self, html):
+    def _partial_url_or_not(self, url):
+        if re.match('(http|https)://', url):
+            return False
+        return True
+
+    def parse_partial_url_to_full(self, partialUrl, baseUrl):
+        if self._partial_url_or_not(partialUrl):
+            # //cdn.xxx.com/avator/2.jpg
+            # to http://cdn.xxx.com/avator/2.jpg
+            if partialUrl.startswith('//'):
+                return urljoin('http:', partialUrl)
+            # /item?abc=123
+            # to http://root/item?abc=123
+            elif partialUrl.startswith('/'):
+                parse_base_url = urlparse(baseUrl)
+                return urljoin('{}://{}'.format(parse_base_url.scheme, parse_base_url.netloc), partialUrl)
+            # item?abc=123
+            # to http://root/toor/item?abc=123
+            else:
+                # http://root/toor/xxx.yyy
+                # to http://root/toor/
+                baseUrl = baseUrl[:baseUrl.rfind('/')+1]
+                return urljoin(baseUrl, partialUrl)
+        return partialUrl
+
+    def get_urls(self, html, baseUrl=''):
         if self.isJson:
             return {}
 
-        if isinstance(self.url_rule, str):
-            urls = set(re.findall(self.url_rule, html))
+        if isinstance(self.urlRule, str):
+            urls = set(re.findall(self.urlRule, html))
         else:
-            urls = set(self.url_rule(html))
+            urls = set(self.urlRule(html))
+
+        urls = (self.parse_partial_url_to_full(i, baseUrl) for i in urls)
+
         return urls
 
     async def analyze_response(self, response):
         if any([i(response) for i in self.rules]) or not self.rules:
-            if self.item is not None: 
+            if self.item is not None:
                 item = self.parse_item(response)
                 try:
                     # whether it is async function or not, it will be run
@@ -57,18 +87,20 @@ class BaseParser(object):
                     # def save():
                     #      print('save me!')
                     # will raise `TypeError: object NoneType can't be used in 'await' expression` but will also output "save me".
-                    # 
+                    #
                     await item.save()
                 except Exception as e:
                     if 'await' in str(e):
-                        logger.error("Get some error when tried to save data, it seems because `item.save()` is not an async function.")
+                        logger.error(
+                            "Got some error when tried to save data, it seems because `item.save()` is not an async function.")
                     else:
-                        logger.error("Get some error when tried to save data, please check again, this is the error information:", exc_info=True)
+                        logger.error(
+                            "Got some error when tried to save data, please check again, this is the error information:", exc_info=True)
 
         if issubclass(self.item, BinItem):
             return set()
 
-        return self.get_urls(response.text)
+        return self.get_urls(response.text, response.url)
 
 
 class Parser(BaseParser):
@@ -80,9 +112,10 @@ class Parser(BaseParser):
         if rule in response.text:
 
     """
-    def __init__(self, item=None, url_rule=None, rule=None, isJson=False):
+
+    def __init__(self, item=None, urlRule=None, rule=None, isJson=False):
         super().__init__(
-            url_rule,
+            urlRule,
             item,
             rule and [lambda response: True if rule in response.text else False],
             isJson)
@@ -97,11 +130,13 @@ class ReParser(BaseParser):
         if re.search(rule, response.text) is not None:
             True
     """
-    def __init__(self, url_rule=None, item=None, rule=None, isJson=False):
+
+    def __init__(self, urlRule=None, item=None, rule=None, isJson=False):
         super().__init__(
-            url_rule,
+            urlRule,
             item,
-            rule and [lambda response: True if re.search(rule, response.text) else False],
+            rule and [lambda response: True if re.search(
+                rule, response.text) else False],
             isJson)
 
 
@@ -113,19 +148,19 @@ class FuncParser(BaseParser):
             if response.url == 'https://github.com':
                 return True
             return False
-        
+
         def function2(response):
             if 'google' in response.url:
                 return True
             return False
 
-        FuncParser([function, function2])
-                
+        FuncParser(rule=[function, function2])
+
     """
-    def __init__(self, url_rule=None, item=None,  rule:iter=None, isJson=None):
+
+    def __init__(self, urlRule=None, item=None,  rule: iter=None, isJson=None):
         super().__init__(
-            url_rule, 
+            urlRule,
             item,
             rule,
             isJson)
-
